2f6f87e110d94ff37448dfaae66c2b693f50f905
Matt Favoino <mattfavoino@gmail.com>
2018-01-18 16:06:34 -0800

    Rewrote how dict created, put output in separate func

--- scrape.py
+++ scrape.py
@@ -6,31 +6,33 @@ import requests
 
 def indeed_search(query, location):
     """Find jobs that match query passed."""
-    print('Be patient.')
     indeed_url = 'http://www.indeed.com/jobs?q={0}&l={1}&sort=date&start='.format(query, location)
-    r = requests.get(indeed_url + query)
+    post_url = 'http://www.indeed.com'
+    r = requests.get(indeed_url)
     soup = BeautifulSoup(r.text, 'lxml')
     organic = soup.find_all('div', {'data-tn-component': 'organicJob'})
+
     companies = [x.span.text.strip() for x in organic]
-    details = [x.h2.a.attrs for x in organic]
-    [details[i].update({'company': companies[i]}) for i, x in enumerate(details)]
+    loc = soup.find_all('span', {'class': 'location'})
+    locations = [x.text for x in loc]
+    all_attrs = [x.h2.a.attrs for x in organic]
+    info = [{'title': x['title'], 'link': post_url + x['href']} for x in all_attrs]
 
-    company = [x['company'] for x in details]
-    title = [x['title'] for x in details]
-    link = [x['href'] for x in details]
+    # add company and location to info dictionary
+    [info[i].update({'company': companies[i]}) for i, x in enumerate(info)]
+    [info[i].update({'location': locations[i]}) for i, x in enumerate(info)]
+    return info
 
-    for i in range(len(company)):
-        print('Company: ' + company[i])
-        print('Title: ' + title[i])
-        print('Link: ' + indeed_url + link[i])
 
-    # for dic in details:
-    #     while :
-    #         print('Company: ' + dic['company'])
-    #         print('Title: ' + dic['title'])
-    #         print('Link: ' + indeed_url + dic['href'])
+def indeed_output(info):
+    """Print jobs fetched from indeed to terminal."""
+    for dic in info:
+        print('----------------------------')
+        for key in dic:
+            print('{}: {}'.format(key, dic[key]))
 
 
 if __name__ == '__main__':
     import sys
     indeed_search(sys.argv[1], sys.argv[2])
+    indeed_output(indeed_search(sys.argv[1], sys.argv[2]))
