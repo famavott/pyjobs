9c95d8a711980d6af70a704b188df2c5217ef0bc
Matt Favoino <mattfavoino@gmail.com>
2018-01-22 11:33:47 -0800

    Reformatted url for adv search, passing cities list to search. About to refactor to print to email and not overwrite dict.

--- scrape.py
+++ scrape.py
@@ -10,23 +10,29 @@ import requests
 import yagmail
 
 
-def indeed_search(query, location):
+def indeed_search(query, cities):
     """Find jobs that match query passed."""
-    indeed_url = 'http://www.indeed.com/jobs?q={0}&l={1}&sort=date&start='.format(query, location)
-    post_url = 'http://www.indeed.com'
-    r = requests.get(indeed_url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    organic = soup.find_all('div', {'data-tn-component': 'organicJob'})
+    for city in cities:
+        indeed_url = (
+            'https://www.indeed.com/jobs?as_and={0}'
+            '&as_phr=&as_any=&as_not=&as_ttl=&as_cm'
+            'p=&jt=fulltime&st=&sr=directhire&salar'
+            'y=&radius=25&l={1}&fromage=1&limit=50&'
+            'sort=date&psf=advsrch').format(query, city).replace(' ', '+')
+        post_url = 'http://www.indeed.com'
+        r = requests.get(indeed_url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        organic = soup.find_all('div', {'data-tn-component': 'organicJob'})
 
-    companies = [x.span.text.strip() for x in organic]
-    loc = soup.find_all('span', {'class': 'location'})
-    locations = [x.text for x in loc]
-    all_attrs = [x.h2.a.attrs for x in organic]
-    info = [{'title': x['title'], 'link': post_url + x['href']} for x in all_attrs]
+        companies = [x.span.text.strip() for x in organic]
+        loc = soup.find_all('span', {'class': 'location'})
+        locations = [x.text for x in loc]
+        all_attrs = [x.h2.a.attrs for x in organic]
+        info = [{'title': x['title'], 'link': post_url + x['href']} for x in all_attrs]
 
-    # add company and location to info dictionary
-    [info[i].update({'company': companies[i]}) for i, x in enumerate(info)]
-    [info[i].update({'location': locations[i]}) for i, x in enumerate(info)]
+        # add company and location to info dictionary
+        [info[i].update({'company': companies[i]}) for i, x in enumerate(info)]
+        [info[i].update({'location': locations[i]}) for i, x in enumerate(info)]
     return info
 
 
@@ -55,19 +61,20 @@ def dict_to_csv(csv_file, columns, info):
     return
 
 
-def send_email():
-    """Send csv to recipient."""
-    try:
-        yag = yagmail.SMTP(os.environ.get('gmail_user'), os.environ.get('gmail_pass'))
-        path = os.getcwd()
-        csv_attachment = path + '/csv/indeed.csv'
-        contents = ['The csv is attached. Open in Google Sheets.']
-        yag.send('keeley.favoino@gmail.com', "Indeed: Accounting Manager", contents, csv_attachment)
-    except Exception:
-        print('An error occured attempting to send the email.')
+# def send_email():
+#     """Send csv to recipient."""
+#     try:
+#         yag = yagmail.SMTP(os.environ.get('gmail_user'), os.environ.get('gmail_pass'))
+#         path = os.getcwd()
+#         csv_attachment = path + '/csv/indeed.csv'
+#         contents = ['The csv is attached. Open in Google Sheets.']
+#         yag.send('keeley.favoino@gmail.com', "Indeed: Accounting Manager", contents, csv_attachment)
+#     except Exception:
+#         print('An error occured attempting to send the email.')
 
 
 if __name__ == '__main__':
-    import sys
-    indeed_output(indeed_search(sys.argv[1], sys.argv[2]))
-    send_email()
+    cities = ['boise', 'chicago', 'denver']
+    indeed_output(indeed_search('accounting manager', cities))
+    # indeed_output(indeed_search(sys.argv[1], sys.argv[2]))
+    # send_email()
