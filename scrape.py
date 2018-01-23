6cd5ee723a88ecfeb3e48d83bd813b8a068ec6a9
Matt Favoino <mattfavoino@gmail.com>
2018-01-22 17:52:45 -0800

    Now combining city lists to use one function call and compile to one csv.

--- scrape.py
+++ scrape.py
@@ -12,53 +12,59 @@ import requests
 import yagmail
 
 
-def indeed_search(query, city):
+def indeed_search(query, cities):
     """Find jobs that match query passed."""
-    indeed_url = (
-        'https://www.indeed.com/jobs?as_and={0}'
-        '&as_phr=&as_any=&as_not=&as_ttl=&as_cm'
-        'p=&jt=fulltime&st=&sr=directhire&salar'
-        'y=&radius=25&l={1}&fromage=1&limit=50&'
-        'sort=date&psf=advsrch').format(query, city).replace(' ', '+')
-    post_url = 'http://www.indeed.com'
-    r = requests.get(indeed_url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    organic = soup.find_all('div', {'data-tn-component': 'organicJob'})
+    info = []
+    for city in cities:
+        indeed_url = (
+            'https://www.indeed.com/jobs?as_and={0}'
+            '&as_phr=&as_any=&as_not=&as_ttl=&as_cm'
+            'p=&jt=fulltime&st=&sr=directhire&salar'
+            'y=&radius=25&l={1}&fromage=1&limit=50&'
+            'sort=date&psf=advsrch').format(query, city).replace(' ', '+')
+        post_url = 'http://www.indeed.com'
+        r = requests.get(indeed_url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        organic = soup.find_all('div', {'data-tn-component': 'organicJob'})
 
-    companies = [x.span.text.strip() for x in organic]
-    loc = soup.find_all('span', {'class': 'location'})
-    locations = [x.text for x in loc]
-    all_attrs = [x.h2.a.attrs for x in organic]
-    info = [{'title': x['title'], 'link': post_url + x['href']} for x in all_attrs]
+        companies = [x.span.text.strip() for x in organic]
+        loc = soup.find_all('span', {'class': 'location'})
+        locations = [x.text for x in loc]
+        all_attrs = [x.h2.a.attrs for x in organic]
+        city_info = [{'title': x['title'], 'link': post_url + x['href']} for x in all_attrs]
 
-    # add company and location to info dictionary
-    [info[i].update({'company': companies[i]}) for i, x in enumerate(info)]
-    [info[i].update({'location': locations[i]}) for i, x in enumerate(info)]
-    return info
+        # add company and location to city_info dictionary
+        [city_info[i].update({'company': companies[i]}) for i, x in enumerate(city_info)]
+        [city_info[i].update({'location': locations[i]}) for i, x in enumerate(city_info)]
+        info.append(city_info)
+    final_list = info[0] + info[1]
+    return final_list
 
 
-def indeed_output(info):
+def indeed_output(final_list):
     """Print jobs fetched from indeed to terminal."""
     jobs_for_email = []
     columns = ['title', 'link', 'company', 'location']
     path = os.getcwd()
     date = f'{datetime.date.today():%m_%d}'
     csv_file = path + f'/csv/{date}_indeed.csv'
-    for dic in info:
+    for dic in final_list:
         for key in dic:
             jobs_for_email.append('{}: {}'.format(key, dic[key]))
     jobs_string = '\n\n'.join(jobs_for_email)
-    dict_to_csv(csv_file, columns, info)
+
+    # calls to write dictionary to csv and then send email
+    dict_to_csv(csv_file, columns, final_list)
     send_email(jobs_string)
 
 
-def dict_to_csv(csv_file, columns, info):
-    """Write info dictionary results to csv file."""
+def dict_to_csv(csv_file, columns, final_list):
+    """Write final_list dictionary results to csv file."""
     try:
         with open(csv_file, 'a') as f:
             writer = csv.DictWriter(f, fieldnames=columns)
             writer.writeheader()
-            for data in info:
+            for data in final_list:
                 writer.writerow(data)
     except IOError:
         print('I/O error')
@@ -82,5 +88,4 @@ def send_email(jobs_string):
 
 
 if __name__ == '__main__':
-    indeed_output(indeed_search('accounting manager', 'chicago'))
-    indeed_output(indeed_search('accounting manager', 'denver'))
+    indeed_output(indeed_search('software engineer', ['chicago', 'denver']))
