2122707a14f5679d213f5f7017dfc602a1b47860
Matt Favoino <mattfavoino@gmail.com>
2018-01-24 21:02:39 -0800

    Added teamwork online, need to factor to clean up code

--- scrape.py
+++ scrape.py
@@ -13,7 +13,7 @@ import yagmail
 
 
 def indeed_search(query, cities, pref_titles):
-    """Find jobs that match query passed."""
+    """Find jobs that match query passed on indeed."""
     info = []
     for city in cities:
         indeed_url = (
@@ -40,6 +40,33 @@ def indeed_search(query, cities, pref_titles):
 
     combined_list = [item for sublist in info for item in sublist]
     return filter_by_title(combined_list, pref_titles)
+
+
+def teamwork_search(pref_titles):
+    """Find jobs that match query passed on teamwork online."""
+    teamwork_url = (r'https://www.teamworkonline.com/jobs-in-sports'
+                    '?utf8=%E2%9C%93&employment_opportunity_search%5Bquery%'
+                    '5D=&employment_opportunity_search%5Blocation%5D%5Bname'
+                    '%5D=&employment_opportunity_search%5Blocation%5D%5Badm'
+                    'inistrative_division%5D=&employment_opportunity_search'
+                    '%5Blocation%5D%5Blatitude%5D=&employment_opportunity_s'
+                    'earch%5Blocation%5D%5Blongitude%5D=&employment_opportu'
+                    'nity_search%5Bexclude_united_states_opportunities%5D=0'
+                    '&employment_opportunity_search%5Bcategory_id%5D=332&co'
+                    'mmit=Search&employment_opportunity_search%5Bcareer_level_id%5D=')
+    post_url = 'https://www.teamworkonline.com'
+    r = requests.get(teamwork_url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    jobs = soup.find_all('li', class_='result-item')
+
+    links = [x.a.attrs for x in jobs]
+    final_dict = [{'link': post_url + x['href']} for x in links]
+    companies = [x.find('div', class_='result-name').text for x in jobs]
+    titles = [x.find('div', class_='result-position').text for x in jobs]
+
+    [final_dict[i].update({'company': companies[i]}) for i, x in enumerate(final_dict)]
+    [final_dict[i].update({'title': titles[i]}) for i, x in enumerate(final_dict)]
+    return final_dict
 
 
 def filter_by_title(combined_list, pref_titles):
@@ -72,8 +99,8 @@ def send_email():
         date = f'{datetime.date.today():%m_%d}'
         csv_attachment = f'/Users/mfavoino/coding_practice/pyjobs/csv/{date}_indeed.csv'
         contents = ['<h3>Open CSV in Google Sheets</h3>']
-        yag.send('keeley.favoino@gmail.com',
-                 f'Indeed Jobs for {date}',
+        yag.send('mattfavoino@gmail.com',
+                 f'Job Results for {date}',
                  contents,
                  attachments=f'/Users/mfavoino/coding_practice/pyjobs/csv/{date}_indeed.csv')
     except Exception:
@@ -88,11 +115,15 @@ def output(final_list):
 
     # calls to write dictionary to csv and then send email
     dict_to_csv(csv_file, columns, final_list)
-    send_email()
 
 
 if __name__ == '__main__':
-    results = (indeed_search('accounting',
-                            ['boise', 'chicago', 'denver'],
-                            ['accounting manager', 'controller']))
-    output(results)
+    pref_titles = ['accounting manager', 'controller', 'financial reporting']
+    cities = ['boise', 'chicago', 'denver']
+
+    indeed_results = (indeed_search('accounting', cities, pref_titles))
+    teamwork_results = teamwork_search(pref_titles)
+
+    output(indeed_results)
+    output(teamwork_results)
+    send_email()
